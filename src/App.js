c3b6405d8318cb946770206b57334a331155cad9
IsaacMFernandes <code.isaacfernandes@gmail.com>
2022-12-19 13:43:53 -0500

    Initial commit of a very standard React template, with some extra stuff taken out.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Actually, you don't have to do that.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
