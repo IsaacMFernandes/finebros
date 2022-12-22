4b477d3e82d8f8b78429db07166710886faa4e54
IsaacMFernandes <code.isaacfernandes@gmail.com>
2022-12-22 00:09:16 -0500

    Added Button and History component with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
+    <div>
+      button press history: {props.allClicks.join(' ')}
     </div>
-  );
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button className='Button' onClick={handleClick}>{text}</button>)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div className='App-body'>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
 }
 
 export default App;
